8f6d9cc749b0b62f46801a6aecd4a5ed3a54a014
Brandon <BrandonLuna2021@u.northwestern.edu>
2021-06-08 20:58:05 -0500

    Frontend-Backend integration done. Display not added. Future work.

--- Final_Project/src/flaskApp.py
+++ Final_Project/src/flaskApp.py
@@ -1,11 +1,7 @@
-from flask import Flask, render_template, request, jsonify, url_for, redirect
-##from flask.ext.session import Session --> , session
+from flask import Flask, render_template, url_for, redirect, request
 from tweetClient import TweetClient
 
 app = Flask(__name__)
-#SESSION_TYPE = 'redis'
-#app.config.from_object(__name__)
-#Session(app)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -13,27 +9,24 @@ def index():
         print("got here")
         ##grab results from query in search bar
         profile_handle = request.form['search_profiles']
-        print("searched for profile handle: ", profile_handle, flush=True)
+        #print("searched for profile handle: ", profile_handle, flush=True)
 
         ##redirect user to list of profiles page
         return redirect(url_for('profiles', profile_name=profile_handle))
+    #else:
+     #   profile_handle = request.args.get('search_profiles')
+      #  return redirect(url_for('profiles', profile_name=profile_handle))
 
     return render_template("index.html")
 
-@app.route('/profiles', methods = ['GET', 'POST'])
+@app.route('/profiles/<profile_name>', methods = ['GET', 'POST'])
 def profiles(profile_name):
     print(f'List of profiles for {profile_name}')
     t = TweetClient(f'{profile_name}')
     ##session['twit_client'] = t
     prof = t.getUser()
-    return render_template("search_results.html", name=profile_name, profile=prof)
-
-@app.route('/tweets', methods = ['GET', 'POST'])
-def tweets():
-    ##tc = session.get('twit_client', None)
-    ##tc.getPopularTweets()
-    return render_template("tweetDisplay.html")
-
+    popular_tweets = t.getPopularTweets()
+    return render_template("search_results.html", name=profile_name, profile=prof, tweets=popular_tweets)
 
 
 if __name__ == "__main__":
--- Final_Project/src/tweetClient.py
+++ Final_Project/src/tweetClient.py
@@ -17,21 +17,29 @@ class TweetClient():
 
     def getPopularTweets(self):
         tweets = []
-        print(self.user)
+        ##print(self.user)
         for tweet in tweepy.Cursor(self.api.search, q=f'from:{self.user}',result_type='popular').items(5):
             tweets.append(tweet)
         #print(tweets)
+        #results = self.processTweets(tweets)
         return tweets
 
     def getUser(self):
         print(self.user)
         profile = self.api.lookup_users(screen_name = self.a.me.screen_name, user_id = [f'{self.user}'])
+        #profile_image = profile['profile_image_url']
         ##print(profile)
         return profile
+
+    def processTweets(self, tl):
+        tweet_texts = []
+        for t in tl:
+            tweet_texts.append(t['text'])
+        return tweet_texts
 
 ###
 ### TEST
 ###
-t = TweetClient('sophherr')
-t.getUser()
-t.getPopularTweets()
+##t = TweetClient('iambeckyg')
+##t.getUser()
+##t.getPopularTweets()
